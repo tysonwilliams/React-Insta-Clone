0d48a904d876b2750c457fb8e8f6a4741cd64bec
Tyson Williams <tyson.j.williams@gmail.com>
2018-06-19 09:17:45 -0600

    Setup main component App.js
    Import dummy data
    Import child components: SearchBar, PostContainer
    Add dummy data to App's state
    Add SearchBar and PostContainer components to App's render method

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import instaData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      instaData: instaData,
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="Insta-Clone-App">React Insta Clone</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchBar />
+        {/* <PostContainer instaData={instaData}/> */}
       </div>
     );
   }
